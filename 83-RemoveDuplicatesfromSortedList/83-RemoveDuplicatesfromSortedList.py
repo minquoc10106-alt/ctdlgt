834aecd37c30f81a7bcc236970ef64e29baf9f88
minquoc10106-alt <minquoc10106@gmail.com>
2026-05-15 10:10:56 +0700

    Create 83-RemoveDuplicatesfromSortedList

---
+++ 83-RemoveDuplicatesfromSortedList/83-RemoveDuplicatesfromSortedList.py
@@ -0,0 +1,12 @@
+# Last updated: 5/15/2026, 10:10:56 AM
+1class Solution(object):
+2    def deleteDuplicates(self, head):
+3        hien_tai = head
+4        while hien_tai and hien_tai.next:
+5            if hien_tai.val == hien_tai.next.val:
+6                # Bỏ qua nút kế tiếp nếu nó trùng giá trị
+7                hien_tai.next = hien_tai.next.next
+8            else:
+9                # Di chuyển sang nút tiếp theo nếu không trùng
+10                hien_tai = hien_tai.next         
+11        return head
