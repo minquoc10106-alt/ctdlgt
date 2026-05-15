e7905153248b59e5bd827dee9ec7837ed2ab2c5c
minquoc10106-alt <minquoc10106@gmail.com>
2026-05-15 10:27:35 +0700

    File updated at 5/15/2026, 10:27:35 AM

--- 121-BestTimetoBuyandSellStock/121-BestTimetoBuyandSellStock.py
+++ 121-BestTimetoBuyandSellStock/121-BestTimetoBuyandSellStock.py
@@ -1,8 +1,12 @@
-# Last updated: 5/15/2026, 10:27:28 AM
+# Last updated: 5/15/2026, 10:27:35 AM
 1class Solution(object):
-2    def maxProfit(self, gia):
-3        loi = 0
-4        for i in range(1, len(gia)):
-5            if gia[i] > gia[i-1]:
-6                loi += gia[i] - gia[i-1]
-7        return loi
+2    def maxProfit(self, gia_ca):
+3        loi_nhuan_max = 0
+4        gia_thap_nhat = float('inf')       
+5        for gia in gia_ca:
+6            if gia < gia_thap_nhat:
+7                gia_thap_nhat = gia
+8            loi_nhuan_hien_tai = gia - gia_thap_nhat
+9            if loi_nhuan_hien_tai > loi_nhuan_max:
+10                loi_nhuan_max = loi_nhuan_hien_tai               
+11        return loi_nhuan_max
