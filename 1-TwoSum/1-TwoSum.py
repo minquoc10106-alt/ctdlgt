74efc1a01b0912ec7889b8a83983729993a928eb
minquoc10106-alt <minquoc10106@gmail.com>
2026-05-15 09:43:46 +0700

    File updated at 5/15/2026, 9:43:46 AM

--- 1-TwoSum/1-TwoSum.py
+++ 1-TwoSum/1-TwoSum.py
@@ -1,17 +1,16 @@
-# Last updated: 5/15/2026, 9:42:18 AM
+# Last updated: 5/15/2026, 9:43:46 AM
 1class Solution(object):
-2    def isPalindrome(self, x):
-3        # Nếu x là số âm hoặc kết thúc bằng 0 (mà không phải là số 0) thì không phải đối xứng
-4        if x < 0 or (x % 10 == 0 and x != 0):
-5            return False
-6
-7        so_dao_nguoc = 0
-8        # Chạy vòng lặp để đảo ngược một nửa số x
-9        while x > so_dao_nguoc:
-10            chu_so_cuoi = x % 10
-11            so_dao_nguoc = so_dao_nguoc * 10 + chu_so_cuoi
-12            x //= 10
-13
-14        # Kiểm tra nếu x bằng số đã đảo ngược (cho số chữ số chẵn)
-15        # Hoặc x bằng so_dao_nguoc // 10 (cho số chữ số lẻ, bỏ chữ số ở giữa)
-16        return x == so_dao_nguoc or x == so_dao_nguoc // 10
+2    def intToRoman(self, num):
+3        bang_tra_cuu = [
+4            (1000, "M"), (900, "CM"), (500, "D"), (400, "CD"),
+5            (100, "C"), (90, "XC"), (50, "L"), (40, "XL"),
+6            (10, "X"), (9, "IX"), (5, "V"), (4, "IV"),
+7            (1 , "I")
+8        ]
+9        ket_qua = ""
+10        for gia_tri, ky_hieu in bang_tra_cuu:
+11            while num >= gia_tri:
+12                ket_qua += ky_hieu
+13                num -= gia_tri
+14                
+15        return ket_qua
