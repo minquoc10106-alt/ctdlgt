6c83055bc638641da7ae182fd46e5f2ad04b8813
minquoc10106-alt <minquoc10106@gmail.com>
2026-05-15 10:32:39 +0700

    File updated at 5/15/2026, 10:32:38 AM

--- 141-LinkedListCycle/141-LinkedListCycle.py
+++ 141-LinkedListCycle/141-LinkedListCycle.py
@@ -1,16 +1,12 @@
-# Last updated: 5/15/2026, 10:32:26 AM
+# Last updated: 5/15/2026, 10:32:38 AM
 1class Solution(object):
-2    def preorderTraversal(self, goc):
-3        if not goc:
-4            return []
-5        kq = []
-6        ngan_xep = [goc]
-7
-8        while ngan_xep:
-9            nut = ngan_xep.pop()
-10            kq.append(nut.val)    
-11            if nut.right:
-12                ngan_xep.append(nut.right)
-13            if nut.left:
-14                ngan_xep.append(nut.left) 
-15        return kq
+2    def hasCycle(self, head):
+3        rua = head
+4        tho = head
+5        
+6        while tho and tho.next:
+7            rua = rua.next         
+8            tho = tho.next.next     
+9            if rua == tho:
+10                return True
+11        return False
