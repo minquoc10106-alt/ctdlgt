e71f90c5d116c6871042c4e7770f179c037646a4
minquoc10106-alt <minquoc10106@gmail.com>
2026-05-15 09:49:22 +0700

    File updated at 5/15/2026, 9:49:22 AM

--- 13-RomantoInteger/13-RomantoInteger.py
+++ 13-RomantoInteger/13-RomantoInteger.py
@@ -1,17 +1,24 @@
-# Last updated: 5/15/2026, 9:49:05 AM
+# Last updated: 5/15/2026, 9:49:22 AM
 1class Solution(object):
-2    def longestCommonPrefix(self, danh_sach_chuoi):
+2    def romanToInt(self, s):
 3        """
-4        :type danh_sach_chuoi: List[str]
-5        :rtype: str
+4        :type s: str
+5        :rtype: int
 6        """
-7        if not danh_sach_chuoi:
-8            return ""
-9        tien_to = danh_sach_chuoi[0]
-10        for i in range(1, len(danh_sach_chuoi)):
-11            chuoi_hien_tai = danh_sach_chuoi[i]
-12            while not chuoi_hien_tai.startswith(tien_to):
-13                tien_to = tien_to[:-1]
-14                if not tien_to:
-15                    return ""       
-16        return tien_to
+7        roman_map = {
+8            'I': 1,
+9            'V': 5,
+10            'X': 10,
+11            'L': 50,
+12            'C': 100,
+13            'D': 500,
+14            'M': 1000
+15        }
+16        total = 0
+17        n = len(s)
+18        for i in range(n):
+19            if i < n - 1 and roman_map[s[i]] < roman_map[s[i+1]]:
+20                total -= roman_map[s[i]]
+21            else:
+22                total += roman_map[s[i]]              
+23        return total
