d396620fdc1fa96aae62663abdd35669b6436f66
minquoc10106-alt <minquoc10106@gmail.com>
2026-05-15 09:57:47 +0700

    Create 28-FindtheIndexoftheFirstOccurrenceinaString

---
+++ 28-FindtheIndexoftheFirstOccurrenceinaString/28-FindtheIndexoftheFirstOccurrenceinaString.py
@@ -0,0 +1,9 @@
+# Last updated: 5/15/2026, 9:57:47 AM
+1class Solution(object):
+2    def strStr(self, nguon, mau):
+3        n = len(nguon)
+4        m = len(mau)
+5        for i in range(n - m + 1):
+6            if nguon[i : i + m] == mau:
+7                return i 
+8        return -1
