5867e44caea8b47f56438497ed5e1c8c78aa03ae
minquoc10106-alt <minquoc10106@gmail.com>
2026-05-15 09:52:12 +0700

    File updated at 5/15/2026, 9:52:12 AM

--- 45-JumpGameII/45-JumpGameII.py
+++ 45-JumpGameII/45-JumpGameII.py
@@ -1,20 +1,24 @@
-# Last updated: 5/15/2026, 9:36:55 AM
-1class Solution(object):
-2    def jump(self, nums):
-3        """
-4        :type nums: List[int]
-5        :rtype: int
-6        """
-7        if len(nums) <= 1:
-8            return 0   
-9        jumps = 0
-10        current_end = 0
-11        farthest = 0
-12        for i in range(len(nums) - 1):
-13            farthest = max(farthest, i + nums[i])
-14            if i == current_end:
-15                jumps += 1
-16                current_end = farthest
-17                if current_end >= len(nums) - 1:
-18                    break             
-19        return jumps
+# Last updated: 5/15/2026, 9:52:12 AM
+1# Definition for singly-linked list.
+2# class ListNode(object):
+3#     def __init__(self, val=0, next=None):
+4#         self.val = val
+5#         self.next = next
+6
+7class Solution(object):
+8    def mergeTwoLists(self, list1, list2):
+9        dummy = ListNode(0)
+10        current = dummy
+11        while list1 and list2:
+12            if list1.val <= list2.val:
+13                current.next = list1
+14                list1 = list1.next
+15            else:
+16                current.next = list2
+17                list2 = list2.next
+18            current = current.next
+19        if list1:
+20            current.next = list1
+21        elif list2:
+22            current.next = list2
+23        return dummy.next
