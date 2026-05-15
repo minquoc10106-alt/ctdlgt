dbd566c8e2ad73ec4b69484dcef469c50f8d935c
minquoc10106-alt <minquoc10106@gmail.com>
2026-05-15 09:46:06 +0700

    File updated at 5/15/2026, 9:46:06 AM

--- 1-TwoSum/1-TwoSum.py
+++ 1-TwoSum/1-TwoSum.py
@@ -1,14 +1,16 @@
-# Last updated: 5/15/2026, 9:45:02 AM
+# Last updated: 5/15/2026, 9:46:06 AM
 1class Solution(object):
-2    def twoSum(self, nums, target):
-3        # Create a dictionary to store the value and its index
-4
-5        prevMap = {} 
-6
-7        for i, n in enumerate(nums):
-8            diff = target - n
-9
-10            if diff in prevMap:
-11                return [prevMap[diff], i]
-12
-13            prevMap[n] = i
+2    def intToRoman(self, num):
+3        bang_tra_cuu = [
+4            (1000, "M"), (900, "CM"), (500, "D"), (400, "CD"),
+5            (100, "C"), (90, "XC"), (50, "L"), (40, "XL"),
+6            (10, "X"), (9, "IX"), (5, "V"), (4, "IV"),
+7            (1 , "I")
+8        ]
+9        ket_qua = ""
+10        for gia_tri, ky_hieu in bang_tra_cuu:
+11            while num >= gia_tri:
+12                ket_qua += ky_hieu
+13                num -= gia_tri
+14                
+15        return ket_qua
