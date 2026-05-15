e98d20ba9650c64e88da723205334bc88894fd67
minquoc10106-alt <minquoc10106@gmail.com>
2026-05-15 09:59:28 +0700

    File updated at 5/15/2026, 9:59:28 AM

--- 35-SearchInsertPosition/35-SearchInsertPosition.py
+++ 35-SearchInsertPosition/35-SearchInsertPosition.py
@@ -1,14 +1,21 @@
-# Last updated: 5/15/2026, 9:58:51 AM
+# Last updated: 5/15/2026, 9:59:28 AM
 1class Solution(object):
-2    def searchInsert(self, mang, tieu_chau):
-3        trai = 0
-4        phai = len(mang) - 1
-5        while trai <= phai:
-6            giua = (trai + phai) // 2          
-7            if mang[giua] == tieu_chau:
-8                return giua
-9            elif mang[giua] < tieu_chau:
-10                trai = giua + 1
-11            else:
-12                phai = giua - 1           
-13        return trai
+2    def isValidSudoku(self, bang):
+3        hang = [set() for _ in range(9)]
+4        cot = [set() for _ in range(9)]
+5        o_vuong = [set() for _ in range(9)]
+6        for r in range(9):
+7            for c in range(9):
+8                so = bang[r][c] 
+9                if so == ".":
+10                    continue
+11                vi_tri_o = (r // 3) * 3 + (c // 3)
+12                if (so in hang[r] or 
+13                    so in cot[c] or 
+14                    so in o_vuong[vi_tri_o]):
+15                    return False
+16                hang[r].add(so)
+17                cot[c].add(so)
+18                o_vuong[vi_tri_o].add(so)
+19                
+20        return True
