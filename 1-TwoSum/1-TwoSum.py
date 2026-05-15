8cf7c3a35a904352ccb8bc37e69b9609e4da457f
minquoc10106-alt <minquoc10106@gmail.com>
2026-05-15 09:39:27 +0700

    Create 1-TwoSum

---
+++ 1-TwoSum/1-TwoSum.py
@@ -0,0 +1,14 @@
+# Last updated: 5/15/2026, 9:39:26 AM
+1class Solution(object):
+2    def twoSum(self, nums, target):
+3        # Create a dictionary to store the value and its index
+4
+5        prevMap = {} 
+6
+7        for i, n in enumerate(nums):
+8            diff = target - n
+9
+10            if diff in prevMap:
+11                return [prevMap[diff], i]
+12
+13            prevMap[n] = i
