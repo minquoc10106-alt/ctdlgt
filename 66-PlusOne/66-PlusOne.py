09a669047647dced199cad69d3b2b24091edf846
minquoc10106-alt <minquoc10106@gmail.com>
2026-05-15 10:09:07 +0700

    File updated at 5/15/2026, 10:09:07 AM

--- 66-PlusOne/66-PlusOne.py
+++ 66-PlusOne/66-PlusOne.py
@@ -1,18 +1,10 @@
-# Last updated: 5/15/2026, 10:08:55 AM
+# Last updated: 5/15/2026, 10:09:07 AM
 1class Solution(object):
-2    def addBinary(self, a, b):
-3        kq = []
-4        nho = 0
-5        i = len(a) - 1
-6        j = len(b) - 1
-7        while i >= 0 or j >= 0 or nho:
-8            tong = nho
-9            if i >= 0:
-10                tong += int(a[i])
-11                i -= 1
-12            if j >= 0:
-13                tong += int(b[j])
-14                j -= 1
-15            kq.append(str(tong % 2))
-16            nho = tong // 2 
-17        return "".join(reversed(kq))
+2    def plusOne(self, mang_so):
+3        n = len(mang_so)      
+4        for i in range(n - 1, -1, -1):
+5            if mang_so[i] < 9:
+6                mang_so[i] += 1
+7                return mang_so
+8            mang_so[i] = 0
+9        return [1] + mang_so
