0a5d7d8366f2839b078c3053793f1fe914e89a13
minquoc10106-alt <minquoc10106@gmail.com>
2026-05-15 10:33:08 +0700

    Create 144-BinaryTreePreorderTraversal

---
+++ 144-BinaryTreePreorderTraversal/144-BinaryTreePreorderTraversal.py
@@ -0,0 +1,16 @@
+# Last updated: 5/15/2026, 10:33:07 AM
+1class Solution(object):
+2    def preorderTraversal(self, goc):
+3        if not goc:
+4            return []
+5        kq = []
+6        ngan_xep = [goc]
+7
+8        while ngan_xep:
+9            nut = ngan_xep.pop()
+10            kq.append(nut.val)    
+11            if nut.right:
+12                ngan_xep.append(nut.right)
+13            if nut.left:
+14                ngan_xep.append(nut.left) 
+15        return kq
