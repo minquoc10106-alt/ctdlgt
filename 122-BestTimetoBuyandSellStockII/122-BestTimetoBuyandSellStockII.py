999cfdeb54960eff6a27220aa69ba1cc4cb39b19
minquoc10106-alt <minquoc10106@gmail.com>
2026-05-15 10:28:10 +0700

    Create 122-BestTimetoBuyandSellStockII

---
+++ 122-BestTimetoBuyandSellStockII/122-BestTimetoBuyandSellStockII.py
@@ -0,0 +1,8 @@
+# Last updated: 5/15/2026, 10:28:10 AM
+1class Solution(object):
+2    def maxProfit(self, gia):
+3        loi = 0
+4        for i in range(1, len(gia)):
+5            if gia[i] > gia[i-1]:
+6                loi += gia[i] - gia[i-1]
+7        return loi
